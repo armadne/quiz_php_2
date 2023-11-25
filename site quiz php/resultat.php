4475b3c143322a574463598f2670473dbd8d8b09
a_mdne <129204408+armadne@users.noreply.github.com>
2023-11-25 16:33:25 +0100

    Add files via upload

---
+++ site quiz php/resultat.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Score & Résultat</title>
+
+    </head>
+    <body style="background-color: bisque;">
+
+    <?php 
+    
+
+    $response1 = $_POST["item1"];
+    $response2 = $_POST["item2"];
+    $response3 = $_POST["item3"];
+
+    if($response3 == true) {
+        echo "Correct";
+    }
+
+
+    
+    
+    ?>
+</html>
